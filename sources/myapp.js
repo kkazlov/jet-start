1a6f12b0911c3fa32c95f1b8e4282114f9d1b022
kkazlov <kkazlov.it@gmail.com>
2021-10-10 14:31:42 +0300

    stage1 - debug:!production

--- sources/myapp.js
+++ sources/myapp.js
@@ -8,7 +8,7 @@ export default class MyApp extends JetApp {
 			id: APPNAME,
 			version: VERSION,
 			router: BUILD_AS_MODULE ? EmptyRouter : HashRouter,
-			debug: true,
+			debug: !PRODUCTION,
 			start: "/top/contacts"
 		};
 
