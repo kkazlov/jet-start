db891ca406a57fc3c24b9f41c0d74f5229eb77f5
kkazlov <kkazlov.it@gmail.com>
2021-10-18 14:38:56 +0300

    stage2 - tables refactoring

--- sources/views/activities.js
+++ sources/views/activities.js
@@ -4,6 +4,7 @@ import activitiesDB from "../models/activitiesDB";
 import activityTypesDB from "../models/activityTypesDB";
 import contactsDB from "../models/contactsDB";
 import Popup from "./popup";
+import TableView from "./tableView";
 
 import "../styles/activities.css";
 
@@ -22,127 +23,10 @@ export default class Activities extends JetView {
 			}
 		};
 
-		const checkCol = {
-			id: "State",
-			header: "",
-			fillspace: 1,
-			css: {"text-align": "center"},
-			sort: "string",
-			template: "{common.checkbox()}",
-			checkValue: "Close",
-			uncheckValue: "Open"
-		};
-
-		const activityTypeCol = {
-			id: "ActivityType",
-			header: [
-				"Activity Type",
-				{
-					content: "selectFilter",
-					compare: (cellValue, filterValue, obj) => +obj.TypeID === +filterValue
-				}
-			],
-			fillspace: 3,
-			sort: "text",
-			collection: activityTypesDB,
-			template({TypeID}) {
-				const activityType = this.collection.getItem(TypeID) || {
-					Value: "",
-					Icon: ""
-				};
-				const {Value, Icon} = activityType;
-				return `${Value} <span class='fas fa-${Icon}'></span>`;
-			}
-		};
-
-		const dueDateCol = {
-			id: "Date",
-			header: ["Due date", {content: "datepickerFilter"}],
-			fillspace: 3,
-			sort: "string",
-			format: webix.i18n.longDateFormatStr
-		};
-
-		const detailsCol = {
-			id: "Details",
-			header: ["Details", {content: "textFilter"}],
-			sort: "string",
-			fillspace: 6
-		};
-
-		const contactCol = {
-			id: "Contact",
-			header: [
-				"Contacts",
-				{
-					content: "selectFilter",
-					compare(cellValue, filterValue, obj) {
-						return +obj.ContactID === +filterValue;
-					}
-				}
-			],
-			sort: "text",
-			fillspace: 3,
-			collection: contactsDB,
-			template({ContactID}) {
-				const contact = this.collection.getItem(ContactID) || {
-					FirstName: "",
-					LastName: ""
-				};
-				const {FirstName, LastName} = contact;
-				return `${FirstName} ${LastName}`;
-			}
-		};
-
-		const editCol = {
-			id: "edit",
-			header: "",
-			fillspace: 1,
-			css: {"text-align": "center"},
-			template: () => "<span class='far fa-edit editIcon table-icon'></span>"
-		};
-
-		const deleteCol = {
-			id: "delete",
-			header: "",
-			fillspace: 1,
-			css: {"text-align": "center"},
-			template: () => "<span class='far fa-trash-alt deleteIcon table-icon'></span>"
-		};
-
-		const table = {
-			view: "datatable",
-			localId: "table",
-			css: "webix_data_border webix_header_border activity-table",
-			columns: [
-				checkCol,
-				activityTypeCol,
-				dueDateCol,
-				detailsCol,
-				contactCol,
-				editCol,
-				deleteCol
-			],
-
-			onClick: {
-				deleteIcon(e, id) {
-					webix
-						.confirm({
-							title: "Delete",
-							text: "Do you want to delete this record? Deleting cannot be undone."
-						})
-						.then(() => {
-							activitiesDB.remove(id);
-						});
-				},
-				editIcon: (e, id) => {
-					this._popup.showWindow({id, mode: "edit", table: "activities"});
-				}
-			}
-		};
+		const datatable = TableView("activities", {activityTypesDB, contactsDB});
 
 		return {
-			rows: [{paddingX: 15, paddingY: 5, cols: [{}, addBtn]}, table]
+			rows: [{paddingX: 15, paddingY: 5, cols: [{}, addBtn]}, datatable]
 		};
 	}
 
@@ -154,5 +38,36 @@ export default class Activities extends JetView {
 		this.on(activitiesDB.data, "onStoreUpdated", (id) => {
 			if (id) table.filterByAll();
 		});
+
+		this.on(table, "onItemClick", (id, e) => {
+			const editIcon = "far fa-edit editIcon table-icon";
+			const className = e.target.className;
+			if (editIcon === className) {
+				this.editIcon(e, id);
+			}
+		});
+
+		this.on(table, "onItemClick", (id, e) => {
+			const editIcon = "far fa-trash-alt deleteIcon table-icon";
+			const className = e.target.className;
+			if (editIcon === className) {
+				this.deleteIcon(e, id);
+			}
+		});
+	}
+
+	deleteIcon(e, id) {
+		webix
+			.confirm({
+				title: "Delete",
+				text: "Do you want to delete this record? Deleting cannot be undone."
+			})
+			.then(() => {
+				activitiesDB.remove(id);
+			});
+	}
+
+	editIcon(e, id) {
+		this._popup.showWindow({id, mode: "edit", table: "activities"});
 	}
 }
--- sources/views/contacts/activities-table.js
+++ sources/views/contacts/activities-table.js
@@ -3,96 +3,12 @@ import {JetView} from "webix-jet";
 import activitiesDB from "../../models/activitiesDB";
 import activityTypesDB from "../../models/activityTypesDB";
 import Popup from "../popup";
+import TableView from "../tableView";
 
 
 export default class ActivitiesTable extends JetView {
 	config() {
-		const checkCol = {
-			id: "State",
-			header: "",
-			fillspace: 1,
-			css: {"text-align": "center"},
-			sort: "string",
-			template: "{common.checkbox()}",
-			checkValue: "Close",
-			uncheckValue: "Open"
-		};
-
-		const activityTypeCol = {
-			id: "ActivityType",
-			header: {
-				content: "selectFilter",
-				compare: (cellValue, filterValue, obj) => +obj.TypeID === +filterValue
-			},
-
-			fillspace: 3,
-			sort: "text",
-			collection: activityTypesDB,
-			template({TypeID}) {
-				const activityType = this.collection.getItem(TypeID) || {
-					Value: "",
-					Icon: ""
-				};
-				const {Value, Icon} = activityType;
-				return `${Value} <span class='fas fa-${Icon}'></span>`;
-			}
-		};
-
-		const dueDateCol = {
-			id: "Date",
-			header: [{content: "datepickerFilter"}],
-			fillspace: 3,
-			sort: "string",
-			format: webix.i18n.longDateFormatStr
-		};
-
-		const detailsCol = {
-			id: "Details",
-			header: [{content: "textFilter"}],
-			sort: "string",
-			fillspace: 6
-		};
-
-		const editCol = {
-			id: "edit",
-			header: "",
-			fillspace: 1,
-			css: {"text-align": "center"},
-			template: () => "<span class='far fa-edit editIcon table-icon'></span>"
-		};
-
-		const deleteCol = {
-			id: "delete",
-			header: "",
-			fillspace: 1,
-			css: {"text-align": "center"},
-			template: () => "<span class='far fa-trash-alt deleteIcon table-icon'></span>"
-		};
-
-		const datatable = {
-			view: "datatable",
-			localId: "activitiesTable",
-			columns: [
-				checkCol,
-				activityTypeCol,
-				dueDateCol,
-				detailsCol,
-				editCol,
-				deleteCol
-			],
-			css: "webix_data_border webix_header_border activity-table",
-			onClick: {
-				deleteIcon: (e, id) => this.deleteIcon(e, id),
-				editIcon: (e, id) => this.editIcon(e, id)
-			},
-			on: {
-				onAfterFilter() {
-					const contactID = this.$scope.getParam("id", true);
-					this.filter(obj => +obj.ContactID === +contactID, "", true);
-				}
-			}
-		};
-
+		const datatable = TableView("contact", {activityTypesDB});
 		const AddActivityBtn = {
 			view: "button",
 			type: "icon",
@@ -110,11 +26,32 @@ export default class ActivitiesTable extends JetView {
 	}
 
 	init() {
+		const table = this.$$("table");
 		this._popup = this.ui(Popup);
+		this.on(table, "onAfterFilter", () => {
+			const contactID = this.getParam("id", true);
+			table.data.filter(obj => +obj.ContactID === +contactID, "", true);
+		});
+
+		this.on(table, "onItemClick", (id, e) => {
+			const editIcon = "far fa-edit editIcon table-icon";
+			const className = e.target.className;
+			if (editIcon === className) {
+				this.editIcon(e, id);
+			}
+		});
+
+		this.on(table, "onItemClick", (id, e) => {
+			const editIcon = "far fa-trash-alt deleteIcon table-icon";
+			const className = e.target.className;
+			if (editIcon === className) {
+				this.deleteIcon(e, id);
+			}
+		});
 	}
 
 	urlChange() {
-		const table = this.$$("activitiesTable");
+		const table = this.$$("table");
 
 		activitiesDB.waitData.then(() => {
 			const contactID = this.getParam("id", true);
