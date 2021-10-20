8d4b719561fb880b32e117a01a898e6f3a0b0b76
kkazlov <kkazlov.it@gmail.com>
2021-10-20 09:23:23 +0300

    stage3 - settings-activity is done

--- sources/models/activityTypesDB.js
+++ sources/models/activityTypesDB.js
@@ -2,7 +2,7 @@ const activityTypesDB = new webix.DataCollection({
 	url: "http://localhost:8096/api/v1/activitytypes/",
 	save: "rest->http://localhost:8096/api/v1/activitytypes/",
 	scheme: {
-		$init: (obj) => {
+		$change: (obj) => {
 			obj.value = obj.Value;
 		}
 	}
--- sources/views/settings/settings-activity.js
+++ sources/views/settings/settings-activity.js
@@ -7,17 +7,20 @@ export default class SettingsActivity extends JetView {
 		const Table = {
 			view: "datatable",
 			localId: "table",
+			editable: true,
 			css: "webix_data_border webix_header_border activity-table",
 			columns: [
 				{
 					id: "Value",
 					header: "Value",
-					fillspace: 6
+					fillspace: 6,
+					editor: "text"
 				},
 				{
 					id: "Icon",
 					header: "Icon",
-					fillspace: 6
+					fillspace: 6,
+					editor: "text"
 				},
 				{
 					id: "delete",
@@ -26,31 +29,77 @@ export default class SettingsActivity extends JetView {
 					css: {"text-align": "center"},
 					template: () => "<span class='far fa-trash-alt deleteIcon table-icon'></span>"
 				}
-			]
+			],
+			onClick: {
+				deleteIcon: (e, id) => this.deleteIcon(e, id)
+			},
+			rules: {
+				Value: webix.rules.isNotEmpty,
+				Icon: webix.rules.isNotEmpty
+			},
+			on: {
+				onBeforeEditStop(state, editor, ignore) {
+					const check = (editor.getValue() !== "");
+					if (!ignore && !check) {
+						this.validateEditor(editor);
+						return false;
+					}
+					return true;
+				}
+			}
 		};
 
 		const AddBtn = {
 			view: "button",
 			value: "Add",
-			css: "webix_primary"
+			css: "webix_primary",
+			click: () => {
+				const form = this.$$("form");
+
+				if (form.validate()) {
+					const values = form.getValues();
+					activityTypesDB.add(values);
+
+					webix.message("A new record has been added");
+					form.clear();
+					form.clearValidation();
+				}
+			}
 		};
 
 		const CancelBtn = {
 			view: "button",
-			value: "Cancel"
+			value: "Cancel",
+			click: () => {
+				const form = this.$$("form");
+
+				form.clear();
+				form.clearValidation();
+			}
 		};
 
 		const Form = {
 			view: "form",
 			localId: "form",
 			elementsConfig: {
-				labelWidth: 120
+				labelWidth: 120,
+				invalidMessage: "Enter the correct value!",
+				bottomPadding: 18
 			},
 			elements: [
-				{view: "text", label: "Activity Value"},
-				{view: "text", label: "Activity Icon"},
+				{view: "text", label: "Activity Value", name: "Value"},
+				{view: "text", label: "Activity Icon", name: "Icon"},
 				{cols: [AddBtn, CancelBtn]}
-			]
+			],
+			rules: {
+				Value: webix.rules.isNotEmpty,
+				Icon: webix.rules.isNotEmpty
+			},
+			on: {
+				onChange() {
+					this.clearValidation();
+				}
+			}
 		};
 
 		return {
@@ -63,5 +112,16 @@ export default class SettingsActivity extends JetView {
 		activityTypesDB.waitData.then(() => {
 			table.data.sync(activityTypesDB);
 		});
+	}
+
+	deleteIcon(e, id) {
+		webix
+			.confirm({
+				title: "Delete",
+				text: "Do you want to delete this record? Deleting cannot be undone."
+			})
+			.then(() => {
+				activityTypesDB.remove(id);
+			});
 	}
 }
